15daa634bb949d073fb9a348be33eae3c4a06775
Kawthar-Shameseldeen <kawtharshameseldeen5@gmail.com>
2025-09-25 11:32:48 +0300

    Getting a single resource

--- src/main/java/com/example/demo/controller/TopicController.java
+++ src/main/java/com/example/demo/controller/TopicController.java
@@ -1,22 +1,24 @@
 package com.example.demo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Arrays;
 @RestController
 public class TopicController {
+  @Autowired
+  private TopicService topicService;
+
+
   @GetMapping("/topics/values")
-public List<List<String>> getAllTopicValues() {
-    List<Topic> topics = List.of(
-        new Topic("spring", "Spring Framework", "Spring Framework Description"),
-        new Topic("java", "Core Java", "Core Java Description"),
-        new Topic("javascript", "Javascript", "Javascript Description")
-    );
-
-    return topics.stream()
-                 .map(t -> List.of(t.getId(), t.getName(), t.getDescription()))
-                 .toList();
-}
-
+    public List<Topic> getAllTopics(){
+        return topicService.getAllTopics();
+    }
+    
+  @GetMapping("/topics/values/{id}")
+    public Topic getTopic(@PathVariable String id){
+        return topicService.getTopic(id);
+    }
 }
